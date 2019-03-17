2241c93520cf59a344da1c5b29f8816b3c9433c2
ssateesh <sateesh.sabbineni@hotmail.com>
2019-03-16 23:54:47 -0400

    vector operations

--- Algebra/test_unitTest.py
+++ Algebra/test_unitTest.py
@@ -1,8 +1,15 @@
 ##https://pythontesting.net/framework/pytest/pytest-introduction/
 #https://exercism.io/my/tracks/r
 import unittest
-from vectors import VectorOperations
-def test_addVector():
-    operations = VectorOperations()
-    #operations.
+#import VectorOperations
+from .vectors import VectorOperations
 
+class VectorOperationsTests(unittest.TestCase):
+
+    def test_addVector(self):
+        operations = VectorOperations()
+        x=[4,5,6]
+        y=[1,2,3]
+        value = operations.addition(x,y)
+        self.assertEqual(len(value),len(x)
+        
--- Algebra/vectors.py
+++ Algebra/vectors.py
@@ -1,10 +1,48 @@
-class VectorOperations:
+import math
+
+class VectorOperations(Object):
     "Vector opreration in class"
     
     #Constructor to initialize
     def __init__(self):
         print("vector operations")
-
+    
     def addition(v,w):
         """Add two vectors"""
         return [v_i+w_i for v_i,w_i in zip(v,w)]
+        
+    def subtraction(v,w):
+        return [v_i-w_i for v_i,w_i in zip(v,w)]
+
+    def vector_sum(vectors):
+        """Sum of all elements"""
+        result=vectors[0]
+        for vector in vectors[1:]:
+            result = addition(result,vector)
+        return result
+
+    def scalar_multiply(c,v):
+        """c is number v is vector"""
+        return [c* v_i for v_i in v]
+    
+    def vector_mean(vectors):
+        n=len(vectors)
+        return scalar_multiply(1/n, vector_sum(vectors))
+
+    def dot(v,w):
+        """dot product of two vectos dot product measures how far the vector v extends in the w direction"""
+        return sum(v_i * w_i for v_i, w_i in zip(v,w))
+
+    def sum_of_squares(v):
+        """v_1*V_1 + ... v_n*v_n"""
+        return dot(v,v)
+        
+    def magnitude(v):
+        return math.sqrt(sum_of_squares(v)) #sqrt: Square root 
+    
+    def squared_distance(v,w):
+        """ditance between two vectos"""
+        return mat.sqrt(subtraction(v,w))
+    
+    def distance(v, w):
+        return math.sqrt(squared_distance(v,w))
